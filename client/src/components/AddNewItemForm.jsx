568748afe1f430215f37740c1cc92307ce5ab53c
Larissa Harada <lharada@primeit.pt>
2025-05-08 12:21:07 +0100

    PLACEHOLDER

--- client/src/components/AddNewItemForm.jsx
+++ client/src/components/AddNewItemForm.jsx
@@ -34,7 +34,7 @@ export function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={(e) => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New Item"
+                    placeholder="What doyou need to do?"
                     aria-label="New item"
                 />
                 <Button
